7a587c474781b760a7fa4b048979acfe8ce80fca
scerelli <s.cerelli@mozestudio.com>
2015-09-03 19:29:59 +0200

    release v1.0.3

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -6,13 +6,34 @@ var App = React.createClass({
     this.refs.modalExample.show();
   },
 
+  beforeOpen: function() {
+    console.log('beforeOpen')
+  },
+
+  beforeClose: function() {
+    console.log('beforeClose')
+  },
+
+  afterOpen: function() {
+    console.log('afterOpen')
+  },
+
+  afterClose: function() {
+    console.log('afterClose')
+  },
+
   render: function() {
     console.log('render');
     return (
       <div className="example">
         <a onClick={this.showModal}>Click To Show Modal</a>
 
-        <ReactModal ref="modalExample" title="Test title modale">
+        <ReactModal ref="modalExample"
+                    title="Test title modale"
+                    beforeOpen={this.beforeOpen}
+                    beforeClose={this.beforeClose}
+                    afterOpen={this.afterOpen}
+                    afterClose={this.afterClose}>
           dwucwu9duhg3vu893rv893rv89hrvh9u8
         </ReactModal>
       </div>
